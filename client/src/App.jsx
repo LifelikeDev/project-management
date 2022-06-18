fec02fc58e68697872d46cae2def0764cfa9af6b
LifelikeDev <primeguyy@gmail.com>
2022-06-18 21:53:45 +0000

    add client on ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import Clients from "./components/Clients";
+import AddClientModal from "./components/AddClientModal";
 
 // create custom cache
 const cache = new InMemoryCache({
@@ -34,6 +35,7 @@ function App() {
       <ApolloProvider client={client}>
         <Header />
         <div className="container">
+          <AddClientModal />
           <Clients />
         </div>
       </ApolloProvider>
